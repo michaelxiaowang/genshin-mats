5d7790191b38c2e3c330e098b6958be2846dda35
Michael Wang <michaelxiaowang@gmail.com>
2020-11-24 17:12:28 -0500

    Updates

---
+++ precache-manifest.4246cb39d1def886fd115e2950459125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4a4d4f9f4d295f76cb76eb054b1238",
+    "url": "/genshin-mats/index.html"
+  },
+  {
+    "revision": "6adeaea3e3f202d4d9a6",
+    "url": "/genshin-mats/static/css/main.16865588.chunk.css"
+  },
+  {
+    "revision": "0fbe54cf6ccf3c3aa10f",
+    "url": "/genshin-mats/static/js/2.f167b04c.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/genshin-mats/static/js/2.f167b04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6adeaea3e3f202d4d9a6",
+    "url": "/genshin-mats/static/js/main.9dfd302d.chunk.js"
+  },
+  {
+    "revision": "1907460956986eff318d",
+    "url": "/genshin-mats/static/js/runtime-main.ffe51f6a.js"
+  }
+]);
